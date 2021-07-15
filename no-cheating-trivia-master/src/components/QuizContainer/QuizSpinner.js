d0cadaf6d60c4619dc69e460e3a5b0babb6de91a
Upinder123 <46994218+Upinder123@users.noreply.github.com>
2021-07-15 19:32:28 +0530

    Add files via upload

---
+++ no-cheating-trivia-master/src/components/QuizContainer/QuizSpinner.js
@@ -0,0 +1,6 @@
+import { Spinner } from "react-bootstrap";
+
+export const spinner =
+  <Spinner className="d-flex mx-auto mt-5" animation="border" role="status" variant="primary">
+    <span className="sr-only">Loading...</span>
+  </Spinner>
