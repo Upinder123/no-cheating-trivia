9cb68542141050e1d101786763f97e93580f4279
Upinder123 <46994218+Upinder123@users.noreply.github.com>
2021-07-15 19:33:44 +0530

    Delete no-cheating-trivia-master directory

--- no-cheating-trivia-master/src/components/CheatingAlert/TestCheating.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CheatingWarning from "./CheatingAlert";
-
-export default function TestCheating() {
-  return <>
-    <CheatingWarning variant="warning" detail="Found opening a new tab"/>
-  </>;
-}
